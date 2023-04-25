cf6d4da1c9d98b2330dca4861868ec0c8e3b9a9c
Oleg Tkach <KratosHome101@gmail.com>
2023-04-25 11:34:16 +0300

    index file

--- src/pages/inbox/inbox.tsx
+++ src/pages/inbox/inbox.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
+const data = [
+    {
+        "id": 1682057751986,
+        "date": "2023-04-07T21:00:00.000Z",
+        "timeStart": "2023-04-07T21:00:00.000Z",
+        "timeEnd": "2023-04-07T21:00:00.000Z",
+        "timer": 1,
+        "title": "New todo",
+        "text": "New todo",
+        "pathMD": "New todo",
+        "completed": false,
+        "notifications": true,
+        "delete": false,
+        subTodo: [
+            {
+                id: 1,
+                "date": "2023-04-07T21:00:00.000Z",
+                "timeStart": "2023-04-07T21:00:00.000Z",
+                "timeEnd": "2023-04-07T21:00:00.000Z",
+                "timer": 1,
+                "title": "New todo",
+                "text": "New todo",
+                "pathMD": "New todo",
+                "completed": false,
+                "notifications": true,
+                "delete": false,
+            }
+        ]
+    }
+]
 
 const Inbox = () => {
-
-
     return (
-        <div>
-
+        <div className="container_inbox">
+            <div>add task (add tim start time continua)</div>
+            <div>focus</div>
+            <div>
+                <div>All task</div>
+                <div>со срокм</div>
+                <div>без срока</div>
+            </div>
+            <div>completed</div>
         </div>
     );
 };
